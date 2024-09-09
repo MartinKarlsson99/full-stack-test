7eef61a871eca32d24fc9d12f1ea1d9851350c3b
Martin Karlsson <martin.karlsson@piktiv.se>
2024-09-09 09:22:46 +0200

    Initial commit

---
+++ backend/src/main/java/com/fullstack/bookmarkbackend/BookmarkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.bookmarkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/fullstack/bookmarkbackend/BookmarkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.bookmarkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
